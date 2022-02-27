21e82691aac6132b658c7292ad8c43f7af559424
Solis <macnwandu@gmail.com>
2022-02-27 16:25:24 -0600

    just changed Ben's code a bit by changing income to expense lol. Also changed the button on hover color and size and the item colors.

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,2 +1,5 @@
 export { default as Navigation } from "./Navigation/Navigation";
 export { default as BudgetNavbar } from "./BudgetNavbar/BudgetNavbar";
+export { default as ExpenseItem } from "./ExpenseItem/ExpenseItem";
+export { default as ExpenseTracker } from "./ExpenseTracker/ExpenseTracker";
+export { default as ExpenseAdder } from "./ExpenseAdder/ExpenseAdder";
--- client/src/pages/Expenses/Expenses.tsx
+++ client/src/pages/Expenses/Expenses.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./Expenses.styles";
+import { ExpenseTracker, ExpenseAdder } from "../../components";
+import { AnimatePresence } from "framer-motion";
 const Expenses = () => {
+  const [displayAdder, setDisplayAdder] = useState<boolean>(false);
+
   return (
-    <Container>
-      <h1>Expenses Page</h1>
+    <Container
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -20, opacity: 0 }}
+      transition={{ duration: 0.35 }}
+      exit={{ opacity: 0, y: -20 }}
+    >
+      <ExpenseTracker setDisplayAdder={setDisplayAdder} />
+      <AnimatePresence>
+        {displayAdder && <ExpenseAdder setDisplayAdder={setDisplayAdder} />}
+      </AnimatePresence>
     </Container>
   );
 };
-
 export default Expenses;
