94af725b5ee9d7c09fbe092849a67b0ff2af28a6
bycho91 <bycho1991@gmail.com>
2022-03-04 18:55:21 -0600

    added changes for the new BE with security. Changed the transaction method route and added the authtoken lines to every axios call. Added the signout method as well.

--- client/src/API/AuthMethods.ts
+++ client/src/API/AuthMethods.ts
@@ -1,31 +1,67 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { LoginData, UserInfoData, URL } from "../constants";
+import { useHistory } from "react-router-dom";
+
+const history = useHistory();
 
 export const signUp = async (userInfo: UserInfoData) => {
   try {
-    await axios.post(`${URL}/auth/signUp`, {
-      email: userInfo.email,
-      password: userInfo.password,
-      first_name: userInfo.firstName,
-      last_name: userInfo.lastName,
-    });
-  } catch (e: any) {
-    // Will need to research more on how to better type the error
-    if (e.response.status > 399) {
-      alert(e.response.data.message[0]);
+    const response = await axios.post(
+      `${URL}/auth/signUp`,
+      {
+        email: userInfo.email,
+        password: userInfo.password,
+        first_name: userInfo.firstName,
+        last_name: userInfo.lastName,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
+      }
+    );
+    sessionStorage.setItem("authToken", response.data.accessToken);
+    if (response) {
+      history.push("/");
     }
+  } catch (e) {
+    const err = e as AxiosError;
+    console.log(err.response?.data?.message);
   }
 };
 
 export const logIn = async (userInfo: LoginData) => {
   try {
-    await axios.post(`${URL}/auth/logIn`, {
-      email: userInfo.email,
-      password: userInfo.password,
-    });
-  } catch (e: any) {
-    if (e.response.status > 399) {
-      alert(e.response.data.message[0]);
+    const response = await axios.post(
+      `${URL}/auth/logIn`,
+      {
+        email: userInfo.email,
+        password: userInfo.password,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
+      }
+    );
+    sessionStorage.setItem("authToken", response.data.accessToken);
+    if (response) {
+      history.push("/");
     }
+  } catch (e) {
+    const err = e as AxiosError;
+    console.log(err.response?.data?.message);
+  }
+};
+
+export const logOut = async () => {
+  try {
+    sessionStorage.setItem("authToken", "");
+    history.push("/login");
+  } catch (e) {
+    const err = e as AxiosError;
+    console.log(err.response?.data?.message);
   }
 };
--- client/src/API/TransactionMethods.ts
+++ client/src/API/TransactionMethods.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { TransactionTransferData, URL, TransactionType } from "../constants";
 
 export const addItem = async (
@@ -7,16 +7,21 @@ export const addItem = async (
 ) => {
   try {
     await axios.post(
-      `${URL}/${type === "expense" ? "Expenses" : "Incomes"}/post${
-        type === "expense" ? "Expense" : "Income"
-      }`,
+      `${URL}/transactions/post${type === "expense" ? "Expense" : "Income"}`,
       {
         title: data.title,
         amount: data.amount,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
       }
     );
-  } catch (err: any) {
-    alert(err.response.data.message[0]);
+  } catch (e) {
+    const err = e as AxiosError;
+    alert(err.response?.data.message);
   }
 };
 
@@ -24,25 +29,37 @@ export const getAllItems = async (type: TransactionType) => {
   try {
     const data = await axios
       .get(
-        `${URL}/${type === "expense" ? "Expenses" : "Incomes"}/all${
-          type === "expense" ? "Expenses" : "Incomes"
-        }`
+        `${URL}/transactions/all${type === "expense" ? "Expenses" : "Incomes"}`,
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+          },
+        }
       )
       .then((res) => res.data);
     return data;
-  } catch (err: any) {
-    alert(err.response.data.message[0]);
+  } catch (e) {
+    const err = e as AxiosError;
+    alert(err.response?.data.message);
   }
 };
 
 export const deleteItem = async (itemId: string, type: TransactionType) => {
   try {
     await axios.delete(
-      `${URL}/${type === "expense" ? "Expenses" : "Incomes"}/delete${
+      `${URL}/transactions/delete${
         type === "expense" ? "Expense" : "Income"
-      }/${itemId}`
+      }/${itemId}`,
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
+      }
     );
-  } catch (err: any) {
-    alert(err.response.data.message[0]);
+  } catch (e) {
+    const err = e as AxiosError;
+    alert(err.response?.data.message);
   }
 };
