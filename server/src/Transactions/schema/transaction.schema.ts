13ef2a3d9e7b0f5476992995b0cc341824c6662d
Jonathan Martinez <Jmartinez070589@gmail.com>
2022-03-08 07:40:25 -0600

    backend edits to passback userId

--- server/src/Transactions/schema/transaction.schema.ts
+++ server/src/Transactions/schema/transaction.schema.ts
@@ -1,19 +1,19 @@
 import * as mongoose from 'mongoose';
 
 export const IncomeSchema = new mongoose.Schema({
-  title: String,
-  amount: Number,
-  type: String,
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, required: true },
   date_posted: String,
   last_date_edited: String,
-  user_id: String,
+  user_id: { type: String, required: true },
 });
 
 export const ExpenseSchema = new mongoose.Schema({
-  title: String,
-  amount: Number,
-  type: String,
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, required: true },
   date_posted: String,
   last_date_edited: String,
-  user_id: String,
+  user_id: { type: String, required: true },
 });
