5560a6a456d50544c776a1b6a4135163ff340517
Benjamin Cho <71800530+bycho91@users.noreply.github.com>
2022-03-10 17:58:41 -0600

    fixed the merge conflicts on home.tsx file (#82)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,88 +1,15 @@
-import React, { useEffect, useState } from "react";
-import {
-  BudgetMainContainer,
-  BudgetTitle,
-  Container,
-  ExpenseBar,
-  TotalBudgetBar,
-  Soft,
-  BudgetInfoContainer,
-  BudgetInfo,
-  BudgetLinksContainer,
-  BudgetLink,
-  PageTitle,
-  AddButton,
-} from "./Home.styles";
-
+import React from "react";
+import { Container, PageTitle } from "./Home.styles";
 import { BudgetData } from "../../dummydata";
-import { BsPlusSquare } from "react-icons/bs";
-import { getAllBudgets, createBudget } from "../../API/BudgetMethods";
 
-const dummy = {
-  month: "January",
-  year: "2022",
-  total: 3000,
-  currentAmount: 500,
-};
+import { BudgetCardList } from "../../components";
 
 const Home = () => {
-  const [budgets, setBudgets] = useState(null);
-  const retrieveBudgets = async () => {
-    const data = await getAllBudgets();
-    setBudgets(data);
-  };
-
-  useEffect(() => {
-    retrieveBudgets();
-  }, []);
-
   return (
-    <>
+    <Container>
       <PageTitle>Your Budgets</PageTitle>
-      {budgets == null ? (
-        <div>Loading...</div>
-      ) : (
-        <Container>
-          {BudgetData.map(({ month, year, spent, budget, active }) => (
-            <BudgetMainContainer key={month} currentmonth={active}>
-              <BudgetTitle>
-                {month} <Soft>{year}</Soft>
-              </BudgetTitle>
-
-              <TotalBudgetBar>
-                <ExpenseBar
-                  percentage={Math.min((spent / budget) * 100, 100)}
-                />
-              </TotalBudgetBar>
-
-              <BudgetInfoContainer>
-                <BudgetInfo>
-                  ${spent} of <Soft>${budget}</Soft>
-                </BudgetInfo>
-                <BudgetInfo>
-                  {`${budget - spent < 0 ? "-" : ""}$${Math.abs(
-                    budget - spent
-                  )}`}{" "}
-                  <Soft>{budget - spent > 0 ? "left" : "over"}</Soft>
-                </BudgetInfo>
-              </BudgetInfoContainer>
-
-              <BudgetLinksContainer>
-                <BudgetLink to="/budget/income" currentmonth={active}>
-                  Adjust Incomes
-                </BudgetLink>
-                <BudgetLink to="/budget/expenses" currentmonth={active}>
-                  Adjust Expenses
-                </BudgetLink>
-              </BudgetLinksContainer>
-            </BudgetMainContainer>
-          ))}
-          <AddButton onClick={() => createBudget(dummy)}>
-            <BsPlusSquare size="2.5rem" />
-          </AddButton>
-        </Container>
-      )}
-    </>
+      <BudgetCardList budgets={BudgetData} />
+    </Container>
   );
 };
 
