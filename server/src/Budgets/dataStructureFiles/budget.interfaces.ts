bd218bee35bd23a1a00ac3134535d1c1c9c12acd
Jonathan Martinez <Jmartinez070589@gmail.com>
2022-03-10 18:46:49 -0600

    added a method to serve up budget titles in the form of 'Month year', also changed data shapes a bit.

--- server/src/Budgets/dataStructureFiles/budget.interfaces.ts
+++ server/src/Budgets/dataStructureFiles/budget.interfaces.ts
@@ -2,10 +2,10 @@ import { Document } from 'mongoose';
 import { MongoDBID } from 'src/shared/types';
 
 export interface BudgetInterface extends Document {
-  readonly month: string;
-  readonly year: string;
+  readonly title: string;
   readonly total: number;
   readonly currentAmount: number;
+  created: boolean;
   date_created: string;
   last_date_edited: String;
   user_id: MongoDBID;
