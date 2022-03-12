20fa2da335095bd4492b29773a969e010a854a52
Jonathan Martinez <Jmartinez070589@gmail.com>
2022-03-12 09:23:13 -0600

    Changes to transactions and a quick budget service fix.

--- server/src/Transactions/transaction.module.ts
+++ server/src/Transactions/transaction.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/Auth/auth.module';
-import { ExpenseSchema, IncomeSchema } from './dataStructureFiles/transaction.schema';
+import { TransactionSchema } from './dataStructureFiles/transaction.schema';
 import { TransactionController } from './transaction.controller';
 import { TransactionServices } from './transaction.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'IncomeSchema', schema: IncomeSchema },
-      { name: 'ExpenseSchema', schema: ExpenseSchema },
+      { name: 'TransactionSchema', schema: TransactionSchema },
     ]),
     AuthModule,
   ],
--- server/src/Transactions/transaction.service.ts
+++ server/src/Transactions/transaction.service.ts
@@ -17,29 +17,23 @@ import { TransactionDTO } from './dataStructureFiles/transaction.dto';
 @Injectable()
 export class TransactionServices {
   constructor(
-    @InjectModel('IncomeSchema')
-    private readonly incomeModel: Model<TransactionInterface>,
-    @InjectModel('ExpenseSchema')
-    private readonly expenseModel: Model<TransactionInterface>
+    @InjectModel('TransactionSchema')
+    private readonly transactionModel: Model<TransactionInterface>
   ) {}
 
   // ALL TRANSACTION METHODS
   async addTransaction(
     incomingDTO: TransactionDTO,
     user: User,
-    transactionType: string
+    budget_id: MongoDBID
   ): Promise<TransactionInterface> {
-    const newTransaction =
-      transactionType === 'income'
-        ? new this.incomeModel(incomingDTO)
-        : new this.expenseModel(incomingDTO);
+    const newTransaction = new this.transactionModel(incomingDTO);
     newTransaction.type =
-      transactionType === 'income'
-        ? IncomeOrExpense.INCOME
-        : IncomeOrExpense.EXPENSE;
+      incomingDTO.amount < 0 ? IncomeOrExpense.EXPENSE : IncomeOrExpense.INCOME;
     newTransaction.date_posted = dateStamp();
     newTransaction.last_date_edited = dateStamp();
     newTransaction.user_id = user._id;
+    newTransaction.budget_id = budget_id;
     return newTransaction.save();
   }
 
@@ -60,13 +54,9 @@ export class TransactionServices {
 
   async getAllTransactions(
     user: User,
-    transactionType: IncomeOrExpense,
     budgetID: MongoDBID
   ): Promise<TransactionInterface[]> {
-    const allTransactions =
-      transactionType === 'income'
-        ? await this.incomeModel.find().exec()
-        : await this.expenseModel.find().exec();
+    const allTransactions = await this.transactionModel.find().exec();
     if (allTransactions) {
       const currentUserTransactions = allTransactions.filter(
         (transaction) =>
@@ -77,28 +67,19 @@ export class TransactionServices {
     throw new UnauthorizedException('User must own transactions');
   }
 
+  // Start here. This one and delete aren't working.
   async editTransaction(
     user: User,
     transactionID: MongoDBID,
-    transactionDTO: TransactionDTO,
-    transactionType: IncomeOrExpense
+    transactionDTO: TransactionDTO
   ): Promise<TransactionInterface> {
-    const editableTransaction =
-      transactionType === 'income'
-        ? await this.incomeModel.findByIdAndUpdate(
-            transactionID,
-            transactionDTO,
-            {
-              new: true,
-            }
-          )
-        : await this.expenseModel.findByIdAndUpdate(
-            transactionID,
-            transactionDTO,
-            {
-              new: true,
-            }
-          );
+    const editableTransaction = await this.transactionModel.findByIdAndUpdate(
+      transactionID,
+      transactionDTO,
+      {
+        new: true,
+      }
+    );
     editableTransaction.last_date_edited = dateStamp();
     if (editableTransaction === null) {
       throw new NotFoundException('Transaction does not exist!');
@@ -111,17 +92,15 @@ export class TransactionServices {
 
   async deleteTransaction(
     user: User,
-    transactionID: MongoDBID,
-    transactionType: IncomeOrExpense
+    transactionID: MongoDBID
   ): Promise<
     TransactionInterface & {
       _id: MongoDBID;
     }
   > {
-    const deletedtransaction =
-      transactionType === 'income'
-        ? await this.incomeModel.findByIdAndRemove(transactionID)
-        : await this.expenseModel.findByIdAndRemove(transactionID);
+    const deletedtransaction = await this.transactionModel.findByIdAndRemove(
+      transactionID
+    );
     if (deletedtransaction === null) {
       throw new NotFoundException('Transaction was already deleted.');
     } else if (user._id.toString() === deletedtransaction.user_id) {
